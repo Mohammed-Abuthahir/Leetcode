88a5ce7f39205297a49be28544e78e45e9c36b45
Mohammed-Abuthahir <mohammedabuthahir214@gmail.com>
2026-02-18 18:24:01 +0530

    DSA

---
+++ Strings/rotateString.java
@@ -0,0 +1,19 @@
+// 796. Rotate String
+import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
+class rotateString{
+    public static boolean rotate(String s,String goal){
+         if(s.length() != goal.length()) return false;
+         return (s + s).indexOf(goal) != -1 ? true : false;
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the s :");
+        String s = scan.nextLine();
+        System.out.println("Enter the goal :");
+        String goal = scan.nextLine();
+        boolean result = rotate(s,goal);
+        System.out.println(result);
+    }
+}
