c7507b09ad8af7c5942dd0ecd78fc5dfc8a0a25f
Mohammed-Abuthahir <mohammedabuthahir214@gmail.com>
2026-03-01 19:15:52 +0530

    DSA

--- Arrays/smallestevenmultiple.java
+++ Arrays/smallestevenmultiple.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 class smallestevenmultiple{
     public static int smallest(int n){
-        return (n & 1) == 0 ? n : n * 2;
+        return n % 2 == 0 ? n : n * 2;
     }
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
