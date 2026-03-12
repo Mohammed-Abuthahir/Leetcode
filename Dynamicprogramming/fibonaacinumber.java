507ca7d4d0cbfd514e846e0dbcedb010dc03e2a1
Mohammed-Abuthahir <mohammedabuthahir214@gmail.com>
2026-03-12 20:22:01 +0530

    DSA

--- Dynamicprogramming/fibonaacinumber.java
+++ Dynamicprogramming/fibonaacinumber.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 import java.util.Arrays;
 class fibonaacinumber{
     public static int fibonacci(int n){
-        // Bottom - up Approach
         if(n <= 1) return n;
         int[] dp = new int[n + 1];
         dp[0] = 0; dp[1] = 1;
@@ -13,10 +12,7 @@ class fibonaacinumber{
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
-        // Top-down approach
-        //  if(n <= 1) return n;
-        // if(dp[n] != 0) return dp[n];
-        // return  dp[n] = fib(n - 1,dp) + fib(n - 2,dp);
+    
     }
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
