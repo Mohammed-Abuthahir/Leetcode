12c2a48eed64155973220b040e46f361c499edad
Mohammed-Abuthahir <mohammedabuthahir214@gmail.com>
2026-04-26 11:06:03 +0530

    DSA

---
+++ Tree/Subtree.java
@@ -0,0 +1,41 @@
+// 572. Subtree of Another Tree
+class Subtree{
+    static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val){
+            this.val = val;
+        }
+        TreeNode(int val,TreeNode left,TreeNode right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+   public static boolean isSameTree(TreeNode a, TreeNode b) {
+        if (a == null && b == null) return true;
+        if (a == null || b == null) return false;
+        if (a.val != b.val) return false;
+        return isSameTree(a.left, b.left) &&
+               isSameTree(a.right, b.right);
+    }
+    public static boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) return false;
+        if (isSameTree(root, subRoot)) return true;
+        return isSubtree(root.left, subRoot) ||
+               isSubtree(root.right, subRoot);
+    }
+    public static void main(String[] args){
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(2);
+        TreeNode subRoot = new TreeNode(4);
+        subRoot.left = new TreeNode(1);
+        subRoot.right = new TreeNode(2);
+        boolean result = isSubtree(root,subRoot);
+        System.out.println(result);
+    }
+}
